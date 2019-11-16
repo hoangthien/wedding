46592f55d94a72ef12ee8eb7d56f48c3960cc45b
hoangthien <hoangthien.uit@gmail.com>
2019-11-16 16:39:12 +0700

    update js

--- js/main.js
+++ js/main.js
@@ -107,7 +107,7 @@ $(document).ready(function() {
 		
 	if( $('#countdown').length ) {
 		// Set the date we're counting down to
-		var countDownDate = Date.parse('10 Nov 2019 18:00:00');
+		var countDownDate = Date.parse('29 Jan 2019 18:00:00');
 		
 		// Update the count down every 1 second
 		var x = setInterval(function() {
